7d22228c6b362ce20e8c75c2dc06ad9b98a49570
w3villa-manish-chaudhary <manish.chaudhary@w3villa.com>
2024-09-01 18:40:47 +0530

    fix oauth login

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -61,27 +61,36 @@ const CustomNavbar = () => {
         </button>
         <div className={`collapse navbar-collapse justify-content-between ${isOpen ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav mx-auto">
+
             <li className="nav-item">
               <Link href="/" legacyBehavior>
                 <a className="nav-link custom-class-navtext">Home</a>
               </Link>
             </li>
-            <li className="nav-item">
-              <Link href="/courses" legacyBehavior>
-                <a className="nav-link custom-class-navtext">Courses</a>
-              </Link>
-            </li>
+
+
+            {user && (
+              <li className="nav-item">
+                <Link href="/courses" passHref legacyBehavior>
+                  <a className="nav-link custom-class-navtext">Courses</a>
+                </Link>
+              </li>
+            )}
+
+
             <li className="nav-item">
               <Link href="/about" legacyBehavior>
                 <a className="nav-link custom-class-navtext">About</a>
               </Link>
             </li>
+
+
           </ul>
           <ul className="navbar-nav align-items-center">
             {user ? (
               <>
                 <li className="nav-item dropdown" ref={dropdownRef}>
-                  <div 
+                  <div
                     className="nav-link custom-class-navtext user-avatar"
                     onClick={() => setDropdownOpen(!dropdownOpen)}
                   >
